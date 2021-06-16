62504e002cfe76e9b7cc361d64f590957d03dec5
maniyounus <45403037+maniyounus@users.noreply.github.com>
2021-06-17 00:39:24 +0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+}
+
+# Create a resource group if it doesn't exist
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+
+  tags = {
+    environment = "production"
+  }
+}
