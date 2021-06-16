62873225b866a05704548af68cbcf033ec9534fa
Muhammad Salman Younus <salman.younus@royalcyber.net>
2021-06-17 01:26:46 +0500

    commit

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource_group_name" {
-  type        = string
-  description = "RG name in Azure"
-}
-
-variable "resource_group_location" {
-  type        = string
-  description = "RG location in Azure"
-}
